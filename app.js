24fc58105ee84394ad0e2f247e48129874ce416b
Armaan Shah <armaanshah96@gmail.com>
2016-04-09 17:26:00 -0500

    put sounds and js in

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 function chords(note,position) {
+
+    
 //                if(note[1] == 7) {
 //                    note = note[0] + '7'.sup();
 //                }
@@ -7,21 +9,38 @@ function chords(note,position) {
 //                sound.play()
 };
 function playProgression() {
+    console.log('function')
     var aud1 = document.getElementById('pos1').innerHTML;
     var aud2 = document.getElementById('pos2').innerHTML;
     var aud3 = document.getElementById('pos3').innerHTML;
     var aud4 = document.getElementById('pos4').innerHTML;
-    var sound1 = aud1 + ".mp3";
-    var sound2 = aud2 + ".mp3";
-    var sound3 = aud3 + ".mp3";
-    var sound4 = aud4 + ".mp3";
-    var play1 = new Audio("../sounds/" + sound1);
-    var play2 = new Audio("../sounds/" + sound2);
-    var play3 = new Audio("../sounds/" + sound3);
-    var play4 = new Audio("../sounds/" + sound4);
-    play1.play();
-    play2.play();
-    play3.play();
-    play4.play();
+    var sound1 = aud1 + ".m4a";
+    var sound2 = aud2 + ".m4a";
+    var sound3 = aud3 + ".m4a";
+    var sound4 = aud4 + ".m4a";
+    var play1 = new Audio("sounds/" + sound1);
+    var play2 = new Audio("sounds/" + sound2);
+    var play3 = new Audio("sounds/" + sound3);
+    var play4 = new Audio("sounds/" + sound4);
+    playSound(play1);
+    console.log("sound1");
+    setTimeout(function(){
+        playSound(play2);
+        console.log("sound2");
+},3500);
 
-}
+        setTimeout(function(){playSound(play3);console.log("sound3");},3500);
+
+    
+    setTimeout(function(){playSound(play4);console.log("sound4");},3500);
+
+    
+
+};
+
+
+function playSound(audio_file) {
+    console.log("working")
+    audio_file.play();
+    console.log("delay happened")
+};
